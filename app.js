a3153fbf9e649d1d4f9f0c7495f1d00138f96099
GangMin Jo <55090008+JoGangMin@users.noreply.github.com>
2019-10-12 23:45:33 +0900

    #2.5 Filling Mode

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext('2d'); 
 const color = document.getElementsByClassName('jsColor');
 const sizeRegulator = document.querySelector('#sizeRegulator');
+const mode = document.querySelector('#jsMode');
 
-canvas.width = 700;
-canvas.height = 700;
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR ='#2c2c2c';
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 700;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 15;
-let painting = false;
 
+let painting = false;
+let fill = false;
 
 
 function startPainting(){
@@ -37,6 +44,7 @@ function onMouseMove(){
 function handleColor(event){
     const changeColor = event.target.style.backgroundColor;
     ctx.strokeStyle = changeColor;
+    ctx.fillStyle = changeColor;
     console.log(ctx.strokeStyle);
         }
  
@@ -45,13 +53,31 @@ function brushSize(){
     ctx.lineWidth = Size;
     console.log(event.target.value);
 }
+ 
+function fillPaint (){
+    if(!fill){ // if fill=false
+        fill = true;
+        mode.innerText = "채우기";
+    }
+    else{
+        fill = false;
+        mode.innerText = "브러쉬";
     
+    }
+}
+
+function fillingCanvas(){
+    if(fill){
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    }
+}
 
 if (canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPaintting);
     canvas.addEventListener("mouseleave",stopPaintting);
+    canvas.addEventListener('click',fillingCanvas);
 }
 
 if(color){
@@ -61,4 +87,8 @@ Array.from(color).forEach(color => { color.addEventListener("click",handleColor)
 
 if(sizeRegulator){
 sizeRegulator.addEventListener('change',brushSize)
+}
+
+if(mode){
+    mode.addEventListener('click',fillPaint)
 }
