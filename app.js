513fc69614255794431bc059b64f4d33876a562b
GangMin Jo <55090008+JoGangMin@users.noreply.github.com>
2019-10-12 19:48:38 +0900

    #2.3 Change Color

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext('2d'); 
+const color = document.getElementsByClassName('jsColor');
 
 canvas.width = 700;
 canvas.height = 700;
 
 let painting = false;
 
-ctx.strockStyle = "#2c2c2c";
+ctx.strockStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
+
 
 function startPainting(){
     painting = true;
@@ -19,7 +21,7 @@ function stopPaintting(){
 }
 
 function onMouseMove(){
- //그림그리기   
+ //그림그리기    
  const x = event.offsetX;
  const y = event.offsetY;
 
@@ -34,12 +36,20 @@ function onMouseMove(){
 
 }
 
+function handleColor(event){
+    const color = event.target.style.backgroundColor;
+    console.log(color);
+    ctx.strockStyle = color;
+        }
+ 
+
 if (canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPaintting);
     canvas.addEventListener("mouseleave",stopPaintting);
 }
-else{
+Array.from(color).forEach(color => { color.addEventListener("click",handleColor)
+});
 
-}
+console.log(ctx.strockStyle)
